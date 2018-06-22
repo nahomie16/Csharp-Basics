2fb53b3b35693d739be8e578d0bb0e89deae6ee3
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-06-21 20:35:57 -0500

    Declaired more Variables

--- Program.cs
+++ Program.cs
@@ -1,26 +1,37 @@
 using System; 
 
+        
 namespace Simple
 { 
-	class Program 
-	{ 
-	
-		static void Main(string [] aargs) 
-		{
-			int heartRate = 85;
-			double deposits = 135002796;
-			const float acceleration = 9.800;
-			float mass = 14.6;
-			double distance = 129.763001;
-			
-			Console.WriteLine("Hello World"); 
+    class Program 
+    { 
+    
+            static void Main(string [] aargs) 
+            {
+                    byte sample1 = 0x3A;
+                    byte sample2 = 58; 
+                    int heartRate = 85;
+                    double deposits = 135002796;
+                    const float acceleration = 9.800F;
+                    float mass = 14.6F;
+                    double distance = 129.763001;			
+                    bool lost = true;
+                    bool expensive = true;
+                    int choice = 2;
 
-		} 
+                    // const char integral = '\u{222B}';
+                    // Console.WriteLine(integral);
+
+                    
+                    Console.WriteLine("Hello World");
 
 
-	} 
+        } 
 
-	
+
+    } 
+
+    
 
 
 
