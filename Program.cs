869e58383b5ffd8fd5c493ed262fe4590dffc13d
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-06-21 19:13:10 -0500

    Declared Variables

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@ namespace Simple
 { 
 	class Program 
 	{ 
-
+	
 		static void Main(string [] aargs) 
 		{
+			int heartRate = 85;
+			double deposits = 135002796;
+			const float acceleration = 9.800;
+			float mass = 14.6;
+			double distance = 129.763001;
+			
 			Console.WriteLine("Hello World"); 
 
 		} 
