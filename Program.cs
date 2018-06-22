b4544968cfe47e5f7abc7107fea88404d4a0dd5f
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-06-21 21:43:07 -0500

    added more If stmts

--- Program.cs
+++ Program.cs
@@ -1,57 +1,47 @@
-using System; 
+using Systemusing  System ; 
 
-        
 namespace Simple
-{ 
-    class Program 
-    { 
-    
-            static void Main(string [] aargs) 
-            {
-                    byte sample1 = 0x3A;
-                    byte sample2 = 58; 
-                    int heartRate = 85;
-                    double deposits = 135002796;
-                    const float acceleration = 9.800F;
-                    float mass = 14.6F;
-                    double distance = 129.763001;			
-                    bool lost = true;
-                    bool expensive = true;
-                    int choice = 2;
-                    const string greeting = "Hello";
-                    string name = "Karen";
-                 if (heartRate >= 40 && heartRate <= 80)
+{
+    class Program
+    {
+        static void Main(string[] aargs)
+        {
+            byte sample1 = 0x3A;
+            byte sample2 = 58;
+            int heartRate = 85;
+            double deposits = 135002796;
+            const float acceleration = 9.800F;
+            float mass = 14.6F;
+            double distance = 129.763001;
+            bool lost = true;
+            bool expensive = true;
+            int choice = 2;
+            const string greeting = "Hello";
+            string name = "Karen";
+
+            float force = mass * acceleration;
+
+            if (heartRate >= 40 && heartRate <= 80)
             {
                 Console.WriteLine("Heart rate is normal.");
-
-            } else { 
-                Console.WriteLine("Heart rate is not normal."); 
-            
             }
-            if (deposits>= 100000000){
+            else
+            {
+                Console.WriteLine("Heart rate is not normal.");
+            }
 
+            if (deposits >= 100000000)
+            {
                 Console.WriteLine("You are exceedingly wealthy.");
-
-            }else {
-
+            }
+            else
+            {
                 Console.WriteLine("Sorry you are so poor.");
             }
 
-
-                    // const char integral = '\u{222B}';
-                    // Console.WriteLine(integral);
-
-                    
-                    Console.WriteLine("Hello World");
-
-
-        } 
-
-
-    } 
-
-    
-
-
-
-}  
+            // const char integral = '\u{222B}';
+            // Console.WriteLine(integral);
+            Console.WriteLine("Hello World");
+        }
+    }
+}
